ccfd631de8a66afa2b1a119bf900fa91bf6e9224
Ivan Castillo <137194587+iacastillo90@users.noreply.github.com>
2026-04-13 21:19:10 -0400

    fix(footer): resuelve el error de React cambiando el prop style de string a objeto

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,137 +1,62 @@
-import "./Footer.css"
+import "./Footer.css";
 
 function Footer() {
-
     return (
-
-        <div class="container my-5">
+        <div className="container-fluid mt-5 p-0">
                 <footer
-                    class="text-center text-lg-start text-white"
-                    style="background-color: #929fba"
+                    className="text-center text-lg-start bg-black border-top border-neon-cyan"
+                    style={{ color: '#0ff' }}
                 >
-                    <div class="container p-4 pb-0">
-
-                        <section class="">
-                            <div class="row">
-
-                                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                                    <h6 class="text-uppercase mb-4 font-weight-bold">
-                                        Company name
+                    <div className="container p-4 pb-0">
+                        <section className="">
+                            <div className="row">
+                                <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                                    <h6 className="text-uppercase mb-4 font-weight-bold text-neon-green">
+                                        NX-Forum Corp
                                     </h6>
-                                    <p>
-                                        Here you can use rows and columns to organize your footer
-                                        content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                                        elit.
+                                    <p className="text-muted-hacker">
+                                        Decentralized node network for elite cybernet operations. Welcome to the grid.
                                     </p>
                                 </div>
 
+                                <hr className="w-100 clearfix d-md-none border-neon-cyan" />
 
-                                <hr class="w-100 clearfix d-md-none" />
-
-
-                                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                                    <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+                                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                                    <h6 className="text-uppercase mb-4 font-weight-bold text-neon-green">Protocols</h6>
                                     <p>
-                                        <a class="text-white">MDBootstrap</a>
+                                        <a className="cyber-link text-neon-cyan text-decoration-none">TCP/IP</a>
                                     </p>
                                     <p>
-                                        <a class="text-white">MDWordPress</a>
+                                        <a className="cyber-link text-neon-cyan text-decoration-none">WebSockets</a>
                                     </p>
                                     <p>
-                                        <a class="text-white">BrandFlow</a>
-                                    </p>
-                                    <p>
-                                        <a class="text-white">Bootstrap Angular</a>
+                                        <a className="cyber-link text-neon-cyan text-decoration-none">Encryption</a>
                                     </p>
                                 </div>
 
+                                <hr className="w-100 clearfix d-md-none border-neon-cyan" />
 
-                                <hr class="w-100 clearfix d-md-none" />
-
-
-                                <hr class="w-100 clearfix d-md-none" />
-
-
-                                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                                    <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                                    <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                                    <p><i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
-                                    <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                                    <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-                                </div>
-
-
-
-                                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                                    <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
-
-                                    <a
-                                        class="btn btn-primary btn-floating m-1"
-                                        style="background-color: #3b5998"
-                                        href="#!"
-                                        role="button"
-                                    ><i class="fab fa-facebook-f"></i
-                                    ></a>
-
-                                    <a
-                                        class="btn btn-primary btn-floating m-1"
-                                        style="background-color: #55acee"
-                                        href="#!"
-                                        role="button"
-                                    ><i class="fab fa-twitter"></i
-                                    ></a>
-
-                                    <a
-                                        class="btn btn-primary btn-floating m-1"
-                                        style="background-color: #dd4b39"
-                                        href="#!"
-                                        role="button"
-                                    ><i class="fab fa-google"></i
-                                    ></a>
-
-
-                                    <a
-                                        class="btn btn-primary btn-floating m-1"
-                                        style="background-color: #ac2bac"
-                                        href="#!"
-                                        role="button"
-                                    ><i class="fab fa-instagram"></i
-                                    ></a>
-
-                                    <a
-                                        class="btn btn-primary btn-floating m-1"
-                                        style="background-color: #0082ca"
-                                        href="#!"
-                                        role="button"
-                                    ><i class="fab fa-linkedin-in"></i
-                                    ></a>
-                                    <a
-                                        class="btn btn-primary btn-floating m-1"
-                                        style="background-color: #333333"
-                                        href="#!"
-                                        role="button"
-                                    ><i class="fab fa-github"></i
-                                    ></a>
+                                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                                    <h6 className="text-uppercase mb-4 font-weight-bold text-neon-green">Comms</h6>
+                                    <p className="text-muted-hacker"><i className="fas fa-home mr-3 text-neon-cyan"></i> Server 10012, US</p>
+                                    <p className="text-muted-hacker"><i className="fas fa-envelope mr-3 text-neon-cyan"></i> root@nx-forum.net</p>
+                                    <p className="text-muted-hacker"><i className="fas fa-phone mr-3 text-neon-cyan"></i> + 01 234 567 88</p>
                                 </div>
                             </div>
                         </section>
-
                     </div>
-
 
                     <div
-                        class="text-center p-3"
-                        style="background-color: rgba(0, 0, 0, 0.2)"
+                        className="text-center p-3 text-neon-green"
+                        style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
                     >
-                        © 2020 Copyright:
-                        <a class="text-white" href="https://mdbootstrap.com/"
-                        >MDBootstrap.com</a
-                        >
+                        © 2077 Copyright: 
+                        <a className="text-neon-cyan text-decoration-none fw-bold ms-2" href="#">
+                            NX-Forum
+                        </a>
                     </div>
-
                 </footer>
         </div>
-
     );
 }
 
